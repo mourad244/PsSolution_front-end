fef71e3b8ed306c7231fb7d61f4b81e842522d6b
mourad244 <mouradvilla2@gmail.com>
2021-05-12 20:24:57 +0200

    add to frontend

--- src/Layout/SideBarData.js
+++ src/Layout/SideBarData.js
@@ -1,65 +1,59 @@
-import React from "react";
-import {
-  FaLayerGroup,
-  FaUsersCog,
-  FaRegObjectGroup,
-  FaShoppingBag,
-  FaSlidersH,
-} from "react-icons/fa";
-import { AiFillDashboard, AiFillDollarCircle } from "react-icons/ai";
-import { RiArrowDownSFill, RiArrowUpSFill } from "react-icons/ri";
+import React from 'react';
+import { FaLayerGroup, FaUsersCog, FaRegObjectGroup, FaShoppingBag, FaSlidersH } from 'react-icons/fa';
+import { AiFillDashboard, AiFillDollarCircle } from 'react-icons/ai';
+import { RiArrowDownSFill, RiArrowUpSFill } from 'react-icons/ri';
 
 export const SideBarData = [
-  {
-    title: "Dashboard",
-    path: "/dashboard",
-    icon: <AiFillDashboard />,
-    iconClosed: <RiArrowDownSFill />,
-    iconOpned: <RiArrowUpSFill />,
-    subNav: [
-      {
-        title: "Users",
-        path: "/users",
-        icon: <FaUsersCog />,
-      },
-      {
-        title: "Devis",
-        path: "/devis",
-        icon: <AiFillDollarCircle />,
-      },
-    ],
-  },
-  {
-    title: "Services",
-    path: "/services",
-    icon: <FaSlidersH />,
-    iconClosed: <RiArrowDownSFill />,
-    iconOpned: <RiArrowUpSFill />,
-    subNav: [
-      {
-        title: "Categories",
-        path: "/servicescategorie",
-        icon: <FaLayerGroup />,
-      },
-    ],
-  },
-  {
-    title: "Products",
-    path: "/products",
-    icon: <FaShoppingBag />,
-    iconClosed: <RiArrowDownSFill />,
-    iconOpned: <RiArrowUpSFill />,
-    subNav: [
-      {
-        title: "Types",
-        path: "/productstype",
-        icon: <FaRegObjectGroup />,
-      },
-      {
-        title: "Categories",
-        path: "/productscategorie",
-        icon: <FaLayerGroup />,
-      },
-    ],
-  },
+	{
+		title: 'Dashboard',
+		path: '/dashboard',
+		icon: <AiFillDashboard />,
+		iconClosed: <RiArrowDownSFill />,
+		iconOpned: <RiArrowUpSFill />,
+		subNav: [
+			{
+				title: 'Users',
+				path: '/users',
+				icon: <FaUsersCog />
+			},
+			{
+				title: 'Devis',
+				path: '/devis',
+				icon: <AiFillDollarCircle />
+			}
+		]
+	},
+	{
+		title: 'Services',
+		path: '/services',
+		icon: <FaSlidersH />,
+		iconClosed: <RiArrowDownSFill />,
+		iconOpned: <RiArrowUpSFill />,
+		subNav: [
+			{
+				title: 'Categories',
+				path: '/servicescategorie',
+				icon: <FaLayerGroup />
+			}
+		]
+	},
+	{
+		title: 'Products',
+		path: '/products',
+		icon: <FaShoppingBag />,
+		iconClosed: <RiArrowDownSFill />,
+		iconOpned: <RiArrowUpSFill />,
+		subNav: [
+			{
+				title: 'Types',
+				path: '/productstype',
+				icon: <FaRegObjectGroup />
+			},
+			{
+				title: 'Categories',
+				path: '/productscategorie',
+				icon: <FaLayerGroup />
+			}
+		]
+	}
 ];
