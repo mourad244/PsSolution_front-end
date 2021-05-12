3ba35eddc2deb783276fa1a0d179f1d05aec4029
mourad244 <mouradvilla2@gmail.com>
2021-05-12 00:21:23 +0000

    add changes

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,166 +1,141 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { getServices, deleteService } from "../services/serviceService";
-import { getServicesCategorie } from "../services/serviceCategorieService";
+import { getServices, deleteService } from '../services/serviceService';
+import { getServicesCategorie } from '../services/serviceCategorieService';
 
-import ServicesTable from "./servicesTable";
-import ServiceForm from "./serviceForm";
-import ListGroup from "../common/listGroup";
-import Pagination from "../common/pagination";
+import ServicesTable from './servicesTable';
+import ServiceForm from './serviceForm';
+import ListGroup from '../common/listGroup';
+import Pagination from '../common/pagination';
 
-import SearchBox from "../common/searchBox";
+import SearchBox from '../common/searchBox';
 
-import { paginate } from "../utils/paginate";
+import { paginate } from '../utils/paginate';
 
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
 
-import _ from "lodash";
+import _ from 'lodash';
 
 class Services extends Component {
-  state = {
-    formDisplay: false,
-    services: [],
-    categories: [],
-    currentPage: 1,
-    pageSize: 10,
-    searchQuery: "",
-    selectedCategorie: null,
-    sortColumn: { path: "name", order: "asc" },
-  };
-  async componentDidMount() {
-    const { data } = await getServicesCategorie();
-    const categories = [{ _id: "", name: "Tous les categories" }, ...data];
+	state = {
+		formDisplay: false,
+		services: [],
+		categories: [],
+		currentPage: 1,
+		pageSize: 10,
+		searchQuery: '',
+		selectedCategorie: null,
+		sortColumn: { path: 'name', order: 'asc' }
+	};
+	async componentDidMount() {
+		const { data } = await getServicesCategorie();
+		const categories = [{ _id: '', name: 'Tous les categories' }, ...data];
 
-    const { data: services } = await getServices();
+		const { data: services } = await getServices();
 
-    this.setState({ services, categories });
-  }
+		this.setState({ services, categories });
+	}
 
-  handleDelete = async (service) => {
-    const originalServices = this.state.services;
-    const services = originalServices.filter((m) => m._id !== service._id);
-    this.setState({ services });
+	handleDelete = async (service) => {
+		const originalServices = this.state.services;
+		const services = originalServices.filter((m) => m._id !== service._id);
+		this.setState({ services });
 
-    try {
-      await deleteService(service._id);
-    } catch (ex) {
-      if (ex.response && ex.response.status === 404)
-        toast.error("service déja supprimé");
-      this.setState({ services: originalServices });
-    }
-  };
+		try {
+			await deleteService(service._id);
+		} catch (ex) {
+			if (ex.response && ex.response.status === 404) toast.error('service déja supprimé');
+			this.setState({ services: originalServices });
+		}
+	};
 
-  handlePageChange = (page) => {
-    this.setState({ currentPage: page });
-  };
-  handleCategorieSelect = (type) => {
-    this.setState({
-      selectedCategorie: type,
-      searchQuery: "",
-      currentPage: 1,
-    });
-  };
+	handlePageChange = (page) => {
+		this.setState({ currentPage: page });
+	};
+	handleCategorieSelect = (type) => {
+		this.setState({
+			selectedCategorie: type,
+			searchQuery: '',
+			currentPage: 1
+		});
+	};
 
-  handleSearch = (query) =>
-    this.setState({
-      searchQuery: query,
-      selectedCategorie: null,
-      currentPage: 1,
-    });
+	handleSearch = (query) =>
+		this.setState({
+			searchQuery: query,
+			selectedCategorie: null,
+			currentPage: 1
+		});
 
-  handleSort = (sortColumn) => {
-    this.setState({ sortColumn });
-  };
+	handleSort = (sortColumn) => {
+		this.setState({ sortColumn });
+	};
 
-  toggleForm = () => {
-    this.setState({
-      formDisplay: !this.state.formDisplay,
-    });
-  };
+	toggleForm = () => {
+		this.setState({
+			formDisplay: !this.state.formDisplay
+		});
+	};
 
-  getPagedData = () => {
-    const {
-      pageSize,
-      currentPage,
-      sortColumn,
-      selectedCategorie,
-      searchQuery,
-      services: allServices,
-    } = this.state;
-    let filtered = allServices;
-    if (searchQuery)
-      filtered = allServices.filter((m) =>
-        m.name.toLowerCase().startsWith(searchQuery.toLowerCase())
-      );
-    else if (selectedCategorie && selectedCategorie._id) {
-      filtered = allServices.filter(
-        (m) => m.categorie._id === selectedCategorie._id
-      );
-    }
-    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+	getPagedData = () => {
+		const { pageSize, currentPage, sortColumn, selectedCategorie, searchQuery, services: allServices } = this.state;
+		let filtered = allServices;
+		if (searchQuery)
+			filtered = allServices.filter((m) => m.name.toLowerCase().startsWith(searchQuery.toLowerCase()));
+		else if (selectedCategorie && selectedCategorie._id) {
+			filtered = allServices.filter((m) => m.categorie._id === selectedCategorie._id);
+		}
+		const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
-    const services = paginate(sorted, currentPage, pageSize);
-    return { totalCount: filtered.length, data: services };
-  };
+		const services = paginate(sorted, currentPage, pageSize);
+		return { totalCount: filtered.length, data: services };
+	};
 
-  render() {
-    const { length: count } = this.state.services;
-    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
-    const { user } = this.props;
+	render() {
+		const { length: count } = this.state.services;
+		const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+		const { user } = this.props;
 
-    if (count === 0)
-      return (
-        <div>
-          <h2>aucun service dans la base de donnée</h2>
-          {user && (
-            <ServiceForm
-              formDisplay={this.state.formDisplay}
-              toggleForm={this.toggleForm}
-            />
-          )}
-        </div>
-      );
+		if (count === 0)
+			return (
+				<div>
+					<h2>aucun service dans la base de donnée</h2>
+					{user && <ServiceForm formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} />}
+				</div>
+			);
 
-    const { totalCount, data: services } = this.getPagedData();
-    return (
-      <div className="row">
-        <div className="col-3">
-          <ListGroup
-            items={this.state.categories}
-            selectedItem={this.state.selectedCategorie}
-            onItemSelect={this.handleCategorieSelect}
-          ></ListGroup>
-        </div>
-        <div className="col">
-          <h3>il ya {totalCount} services dans la base de données</h3>
+		const { totalCount, data: services } = this.getPagedData();
+		return (
+			<div className="row">
+				<div className="col-3">
+					<ListGroup
+						items={this.state.categories}
+						selectedItem={this.state.selectedCategorie}
+						onItemSelect={this.handleCategorieSelect}
+					></ListGroup>
+				</div>
+				<div className="col">
+					<h3>il ya {totalCount} services dans la base de données</h3>
 
-          {user && (
-            <ServiceForm
-              formDisplay={this.state.formDisplay}
-              toggleForm={this.toggleForm}
-            />
-          )}
-          <SearchBox
-            value={searchQuery}
-            onChange={this.handleSearch}
-          ></SearchBox>
-          <ServicesTable
-            user={user}
-            services={services}
-            sortColumn={sortColumn}
-            onDelete={this.handleDelete}
-            onSort={this.handleSort}
-          ></ServicesTable>
-          <Pagination
-            itemsCount={totalCount}
-            pageSize={pageSize}
-            currentPage={currentPage}
-            onPageChange={this.handlePageChange}
-          ></Pagination>
-        </div>
-      </div>
-    );
-  }
+					{user && <ServiceForm formDisplay={this.state.formDisplay} toggleForm={this.toggleForm} />}
+					<SearchBox value={searchQuery} onChange={this.handleSearch}></SearchBox>
+					<ServicesTable
+						user={user}
+						services={services}
+						sortColumn={sortColumn}
+						onDelete={this.handleDelete}
+						onSort={this.handleSort}
+					></ServicesTable>
+					<Pagination
+						itemsCount={totalCount}
+						pageSize={pageSize}
+						currentPage={currentPage}
+						onPageChange={this.handlePageChange}
+					></Pagination>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Services;
